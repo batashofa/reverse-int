0dcef19fc7e5892bafd506ca11be0f475b6e30bb
batashofa <batashofa@gmail.com>
2021-07-20 16:01:05 +0300

    feat: added reverse int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let a = [];
+    if (n<0) {
+        a = n.toString().split('').reverse().slice(0, a.length - 1);
+    } else {
+        a = n.toString().split('').reverse();
+    }
+    return +a.join('');
 }
